965086108a468eaba77120c55c56fdc485c35649
Erwan IQUEL <erwan.iquel@gmail.com>
2018-02-06 11:52:25 +0100

    TP2 - Exercice 3 ok

---
+++ tp2/ex3/src/main.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <omp.h>
+
+int main(int argc, char* argv[]) {
+    char* set;
+    int size, i, j;
+
+    if(argc < 2) {
+        printf("Veuillez entrer un nombre (supérieur à 3): \n");
+        scanf("%d", &size);
+    } else {
+        size = atoi(argv[1]);
+    }
+
+
+    if(size < 3) {
+        fprintf(stderr, "Error");
+        exit(-1);
+    }
+
+    set = (char*) malloc(sizeof (char) * size);
+
+    for(i = 0; i < size; i++) {
+        set[i] = 1;
+    }
+
+    set[0] = set[1] = 0;
+
+    omp_set_num_threads(4);
+
+    #pragma omp parallel shared(set) private(i, j)
+    {
+        #pragma omp for schedule(static)
+        for(i = 2; i < size; i++) {
+            if(set[i]) {
+                for(j = i*2; j < size; j+=i) {
+                    set[j] = 0;
+                }
+            }
+        }
+    }
+
+    printf("La liste des nombre premier: \n");
+    for(i = 0; i < size; i++) {
+        if(set[i]) {
+            printf("%d\n", i);
+        }
+    }
+
+    free(set);
+
+    return 0;
+}
