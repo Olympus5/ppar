9b6351a744f9b0848e3946c54a4e8e8d84e87db1
Erwan IQUEL <erwan.iquel@gmail.com>
2018-02-20 10:43:35 +0100

    TP3 - Part1 ok

---
+++ tp2/ex4/inc/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define ALPHABET_SIZE 27
+
+typedef struct char_counter char_counter;
+
+struct char_counter {
+    char _char;
+    int count;
+};
+
+#endif
--- tp2/ex4/src/main.c
+++ tp2/ex4/src/main.c
@@ -1,9 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <omp.h>
+#include "main.h"
 
 int main(int argc, char* argv[]) {
-    
+    char alphabet[ALPHABET_SIZE] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '\0'};
+
+
 
     return 0;
 }
