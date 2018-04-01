6f4cf9b1ab532f206499260b72552928e2c469b8
Erwan IQUEL <erwan.iquel@gmail.com>
2018-04-01 17:29:10 +0200

    I had ident the src code :)

--- tp5/gpu_lab1/summation_kernel.cu
+++ tp5/gpu_lab1/summation_kernel.cu
@@ -5,12 +5,12 @@ __global__ void summation_kernel(int data_size, float * data_out)
 	unsigned int id = blockIdx.x * blockDim.x + threadIdx.x;
 
 	if(id < data_size) data_out[id] += ((id % 2 != 0) ? -1:1) / (id + 1.0);
-//	printf("%d>%f\n",id, data_out[id]);
+	//	printf("%d>%f\n",id, data_out[id]);
 }
 
 __global__ void reduce(int data_size, float* data_in, float* data_out) {
 	extern __shared__ float sdata[];
-
+	
 	unsigned int tid = threadIdx.x;
 	unsigned int id = blockIdx.x * blockDim.x + threadIdx.x;
 
@@ -24,7 +24,7 @@ __global__ void reduce(int data_size, float* data_in, float* data_out) {
 			sdata[index] += sdata[index + s];
 		}
 		__syncthreads();
-	} 	
-	
+	}
+
 	if(tid == 0) data_out[blockIdx.x] = sdata[0];
 }
