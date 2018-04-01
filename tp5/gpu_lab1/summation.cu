b31a3f96cdea0d07f4a3a72ed0d6f7215364cdbc
Erwan IQUEL <erwan.iquel@gmail.com>
2018-04-01 16:42:55 +0200

    TP5 work fine :)

--- tp5/gpu_lab1/summation.cu
+++ tp5/gpu_lab1/summation.cu
@@ -10,7 +10,7 @@ float log2_series(int n)
 
 	for(int i = 0; i < n; i++) {
 		if(i% 2 == 0) {
-			res += 1.0f/(i+1);
+			res += 1.f/(i+1);
 		} else {
 			res -= 1.0f/(i+1);
 		}		
@@ -33,11 +33,11 @@ int main(int argc, char ** argv)
     printf(" time=%fs\n", end_time - start_time);
     
     // Parameter definition
-    int threads_per_block = 4 * 32;
-    int blocks_in_grid = 8;
-    
-    int num_threads = threads_per_block * blocks_in_grid;
+    int threads_per_block = 64;
+    int blocks_in_grid = 2;
 
+    int num_threads = threads_per_block * blocks_in_grid;
+    
     // Timer initialization
     cudaEvent_t start, stop;
     CUDA_SAFE_CALL(cudaEventCreate(&start));
@@ -48,8 +48,10 @@ int main(int argc, char ** argv)
     float* data_out_cpu = (float*) malloc(results_size * sizeof(float));
 	// Allocating output data on GPU
 	float* data_out_gpu;
-	cudaMalloc((void**)&data_out_gpu, results_size * sizeof (float));	
-    // Start timer
+	float*  data_out;
+	cudaMalloc((void**)&data_out_gpu, results_size * sizeof (float));
+	cudaMalloc((void**)&data_out, results_size * sizeof (float)); 
+// Start timer
     CUDA_SAFE_CALL(cudaEventRecord(start, 0));
 
     // Execute kernel
@@ -58,12 +60,29 @@ int main(int argc, char ** argv)
     CUDA_SAFE_CALL(cudaEventRecord(stop, 0));
     CUDA_SAFE_CALL(cudaEventSynchronize(stop));
     // Get results back
-	cudaMemcpy(data_out_cpu, data_out_gpu, results_size * sizeof (float), cudaMemcpyDeviceToHost);   
-    // Finish reduction
+ 	int smemSize = threads_per_block * sizeof(float);
+
+	while(blocks_in_grid > 1) {
+		reduce<<<blocks_in_grid, threads_per_block, smemSize>>>(data_size, data_out_gpu, data_out);
+		data_out_gpu = data_out;
+		threads_per_block = blocks_in_grid;
+		
+		if(blocks_in_grid >= threads_per_block) {
+			blocks_in_grid /= threads_per_block;
+		} else {
+			blocks_in_grid /= blocks_in_grid;
+		}
+	} 
+	
+	// Finish reduction
 	float sum = 0.;
-       for (int i = 0 ; i<results_size; i++){
+	cudaMemcpy(data_out_cpu, data_out_gpu, results_size * sizeof (float), cudaMemcpyDeviceToHost);   
+       	sum = data_out_cpu[0];
+	/*for (int i = 0 ; i<results_size; i++){
 		sum += data_out_cpu[i];
-	}
+		printf("%d\n",i);
+	}*/
+
     // Cleanup
 	cudaFree(data_out_gpu);   
 	free(data_out_cpu);
--- tp5/gpu_lab1/summation_kernel.cu
+++ tp5/gpu_lab1/summation_kernel.cu
@@ -8,3 +8,23 @@ __global__ void summation_kernel(int data_size, float * data_out)
 //	printf("%d>%f\n",id, data_out[id]);
 }
 
+__global__ void reduce(int data_size, float* data_in, float* data_out) {
+	extern __shared__ float sdata[];
+
+	unsigned int tid = threadIdx.x;
+	unsigned int id = blockIdx.x * blockDim.x + threadIdx.x;
+
+	sdata[tid] = (id < data_size) ? data_in[id] : 0;
+	__syncthreads();
+
+	for(unsigned int s = 1; s < blockDim.x; s *= 2) {
+		int index = 2 * s * tid;
+
+		if(index < blockDim.x) {
+			sdata[index] += sdata[index + s];
+		}
+		__syncthreads();
+	} 	
+	
+	if(tid == 0) data_out[blockIdx.x] = sdata[0];
+}
