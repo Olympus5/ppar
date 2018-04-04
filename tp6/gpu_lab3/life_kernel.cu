c48ec65ba7ff4e067333ede57f034d651a8820c6
Erwan IQUEL <erwan.iquel@gmail.com>
2018-04-04 11:00:39 +0200

    1st question work :)

---
+++ tp6/gpu_lab3/life_kernel.cu
@@ -0,0 +1,58 @@
+
+// Reads a cell at (x+dx, y+dy)
+__device__ int read_cell(int * source_domain, int x, int y, int dx, int dy,
+    unsigned int domain_x, unsigned int domain_y)
+{
+    x = (unsigned int)(x + dx) % domain_x;	// Wrap around
+    y = (unsigned int)(y + dy) % domain_y;
+    return source_domain[y * domain_x + x];
+}
+
+// Compute kernel
+__global__ void life_kernel(int * source_domain, int * dest_domain,
+    int domain_x, int domain_y)
+{
+    int tx = blockIdx.x * blockDim.x + threadIdx.x;
+    int ty = blockIdx.y;
+    
+    // Read cell
+    int myself = read_cell(source_domain, tx, ty, 0, 0,
+	                       domain_x, domain_y);
+    	int blue = 0; // Nombre de pion bleu
+	int red = 0; // Nombre de pion rouge
+	int total = 0; // Nombre total de voisin
+	int current; // Position initial du voisin i
+	int dx; // Decalage en x
+	int dy; // Decalage en y
+    // TODO: Read the 8 neighbors and count number of blue and red
+
+	//TODO: NE SUPPRIME RIEN - ERWAN
+	for(int x = 0; x < 3; x++) {
+		dx = (tx + x) % domain_x;
+		for(int y = 0; y < 3; y++) {
+			dy = (ty + y) % domain_y;
+			current = read_cell(source_domain, tx, ty, dx, dy, domain_x, domain_y);
+
+			if(current == 2) {
+				blue++;
+			} else if(current == 1){
+				red++;
+			}		
+		}
+	}
+
+	total = blue + red;
+	// TODO: Compute new value
+	// TODO: NE SUPPRIME TOUJOURS RIEN - ERWAN
+	if(total < 2 ||total > 3 ||(myself == 0&&total != 3)) {//Je meurs !!!!!!!
+		myself = 0;
+	} else {// Je vie
+		if(myself == 0) {
+			myself = (red > blue) ? 1 : 2;
+		}
+	}	
+	// TODO: Write it in dest_domain
+	// TODO: NE SUPPRIME TOUJOURS RIEN - ERWAN
+	dest_domain[ty * domain_x + tx] = myself;		
+}
+
